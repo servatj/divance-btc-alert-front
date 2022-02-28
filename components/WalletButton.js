112b3b2188cc96fedcdb1e401aa256ad0db83659
Josep Servat <jservat@outlook.es>
2022-03-01 00:40:06 +0100

    Improve wallet button style

--- components/WalletButton.js
+++ components/WalletButton.js
@@ -1,6 +1,6 @@
 const WalletButton = ({ connected, connectLogic }) => {
   return (
-    <button onClick={connectLogic}  className="inline-flex items-center justify-center h-16 px-10 py-0 text-xl font-semibold text-center text-black no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-purple-200 hover:border-purple-200 focus:shadow-xs focus:no-underline">
+    <button onClick={connectLogic}  className="inline-flex items-center justify-center h-10 px-10 py-0 text-xl font-semibold text-center text-black no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-purple-200 hover:border-purple-200 focus:shadow-xs focus:no-underline">
       {connected ? 'Wallet Connected' : 'Wallet Connect'}
     </button>
   );
