c19f4b359b238538e4627a1d76ac23234e4c97c8
Josep Servat <jservat@outlook.es>
2022-03-09 00:11:29 +0100

    indicate chain name

--- pages/nft-viewer.js
+++ pages/nft-viewer.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
 import NFTBalance from 'components/NFTBalance';
+import { useMoralis } from 'react-moralis';
+import { getChainNameById } from 'lib/networks';
 
 export default function Home({ dataProps, rows }) {
+  const { chainId } = useMoralis();
+
   return (
     <div className="flex flex-col py-10 bg-purple-600">
       <Head>
@@ -11,7 +15,10 @@ export default function Home({ dataProps, rows }) {
 
       <div className="flex flex-col">
         <div className="flex justify-center align-center">
-          <h1 className="font-bangers text-3xl text-white"> 🖼 Nft Viewer </h1>
+          <h1 className="font-bangers text-3xl text-white">
+            {' '}
+            🖼 Nft Viewer {getChainNameById(chainId)}{' '}
+          </h1>
         </div>
         <div className="flex justify-center align-center">
           <div className="">
